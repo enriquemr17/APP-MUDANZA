98a9dee3ab8027a049064c17e66a5b4a606d7c2e
enriquemr17 <enriquemartinezrodriguez@gmail.com>
2026-05-04 12:26:29 +0200

    actualizacion: añadir botones de reserva y confirmacion de reserva a conductores

--- app/src/main/java/com/example/appmudanza/ui/theme/screens/conductoresDetalladoScreen.kt
+++ app/src/main/java/com/example/appmudanza/ui/theme/screens/conductoresDetalladoScreen.kt
@@ -10,19 +10,31 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.appmudanza.R
 import com.example.appmudanza.data.entity.Vehicle
+import kotlinx.coroutines.launch
 
 
 @Composable
@@ -32,7 +44,12 @@ fun ConductorDetalladoScreen(
     onBack: () -> Unit,
 
 ) {
+    var showDatePicker by remember { mutableStateOf(false) }
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope ()
+
     Scaffold(
+        snackbarHost = { SnackbarHost(snackbarHostState) },
         topBar = {
             TopAppBar(
                 title = { Text("Conductor") },
@@ -79,6 +96,31 @@ fun ConductorDetalladoScreen(
 
             Spacer(modifier = Modifier.height(8.dp))
 
+            Button(onClick = {showDatePicker = true}) {
+                Text("Reservar cita")
+            }
+
+            if (showDatePicker) {
+                val datePickerState = rememberDatePickerState()
+                DatePickerDialog(
+                    onDismissRequest = {showDatePicker = false }, // cierra el calendario
+                    confirmButton = {
+                        Button(onClick = {val  selectedDate = datePickerState.selectedDateMillis
+                            scope.launch { snackbarHostState.showSnackbar("Reserva confirmada") }
+                            showDatePicker= false}) {
+                            Text("Confirmar")
+                        }
+
+                    },
+                    dismissButton = {
+                        Button(onClick = {showDatePicker = false}) {
+                            Text("Cancelar")
+                        }
+                    }
+                ) {
+                    DatePicker(state = datePickerState)
+                }
+            }
         }
     }
 }
